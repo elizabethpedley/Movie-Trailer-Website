b318e023c1c02a48de1ace5ea35182d267802f09
Liz <elizabethmpedley@gmail.com>
2017-07-04 19:35:35 -0500

    fixed issues for second review

--- media.py
+++ media.py
@@ -1,13 +1,14 @@
 import webbrowser
 
-class Movie():
-    """ This is a class where we can store movie data """
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+class Movie():
+    ''' The class Movie() stores information about a particular movie. '''
+
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
+        ''' This contructer function take the a movies title, storyline, poster
+         image, and trailer video and stores them as instance variables. '''
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
-        def show_trailer(self):
-            webbrowser.open(self.trailer_youtube_url)
